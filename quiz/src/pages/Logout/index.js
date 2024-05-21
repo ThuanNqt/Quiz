2a32d5e869365ce25856fa9508151d823afb5e57
Nguyen Quang Thuan <thuan13112003@gmail.com>
2024-05-21 23:31:02 +0700

    Logout

--- quiz/src/pages/Logout/index.js
+++ quiz/src/pages/Logout/index.js
@@ -1,5 +1,11 @@
-import React from "react";
-
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { deleteAllCookies } from "../../helpers/cookie";
 export default function Logout() {
-  return <div>index</div>;
+  const navigate = useNavigate();
+  useEffect(() => {
+    deleteAllCookies();
+    navigate("/login");
+  }, []);
+  return <></>;
 }
